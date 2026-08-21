80a56d83b67d1de20142be5b3552c947147deab7
JulianoDacol <150298546+JulianoDacol@users.noreply.github.com>
2026-08-20 21:46:36 -0300

    Add files via upload
    commitado

---
+++ aula3juliano.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+ {
+//parte 01 operadores relacionais
+  printf("Parte 1 - operadores relacionais \n");
+  printf("7 == 9 Resultado %d\n", 7 == 9);
+  printf("15 != 10 Resultado %d\n", 15 != 10);
+  printf("20 > 25 Resultado %d\n", 20 > 25);
+  printf("20 < 25  Resultado %d\n", 20 < 25);
+  printf("10 >= 5  Resultado %d\n", 10 >= 5);
+  printf("10 <= 5 Resultado %d\n", 10 <= 5);
+  printf("100 == 100  Resultado %d\n", 100 == 100);
+  printf("3 > 3  Resultado %d\n", 3 > 3);
+  printf("3  != 50  Resultado %d\n", 50 != 50);
+  printf("50 != 50  Resultado %d\n", 50 != 50);
+  printf("50 != 50  Resultado %d\n", 50 != 50);
+  printf("50 != 50  Resultado %d\n", 50 != 50);
+  printf("50 != 50  Resultado %d\n", 50 != 50);
+  printf("50 != 50  Resultado %d\n", 50 != 50);
+//parte 02 operador de modulo e comparacoes
+  printf("Parte 02 - operador modulo %% e comparacoes\n");
+  printf("10 %% 2 == 0 Resultado %d\n", 10 % 2);
+  printf("11 %% 2 == 0 Resultado %d\n", 10 % 2);
+  printf("15 %% 3  Resultado %d\n", 15 % 3);
+  printf("17 %% 5 == 2 Resultado %d\n", 17 % 5);
+  printf("20 %% 4 != 0 Resultado %d\n", (20 % 4) != 0);
+  printf("9 %% 2 != 0 Resultado %d\n", (9 % 2) != 2);
+  printf("25 %% 5 == 0 Resultado %d\n", (25 % 5) == 0);
+  printf("18 %% 7 == 4 Resultado %d\n", (18 % 7) == 4);
+/*  printf("21 %% 3 == 0 Resultado %d\n", () );
+  printf("50 != 50  Resultado %d\n", 50 != 50) */
+  
+  printf("Parte 03 - operadorlogico and &&\n");
+  printf("5 > 3 && 10 > 5 Resultado %d\n ", 5 > 3 && 10 > 5);
+
+}
